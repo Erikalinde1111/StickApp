cffee46b792581d140c6a5657a65eac725bc3a10
Erika Linde <erika.j.linde@gmail.com>
2017-12-04 19:47:24 +0100

    lagt till lista med bilder i designs

--- StickApp/components/designs.js
+++ StickApp/components/designs.js
@@ -3,17 +3,43 @@ import {
   StyleSheet,
   Text,
   View,
-  SectionList,
+  FlatList,
   TouchableHighlight
 } from 'react-native';
+import { List, ListItem } from 'react-native-elements';
 
 const Designs = () => {
+
+  data=[
+      {key: 1, title: 'kortärmad', body: '1.0', avatar_url: (require('../pic/banan-.jpg'))},
+      {key: 2, title: 'halvlång', body: '0.5', avatar_url: (require('../pic/banan-.jpg'))},
+      {key: 3, title: 'helång', body: '0.25', avatar_url: (require('../pic/banan-.jpg'))}
+  ];
+
+
+
   return(
     <View>
       <Text>Välj typ av design du vill sticka</Text>
+        <List>
+            <FlatList
+              data={this.data}
+              renderItem={({item}) => (
+                <ListItem
+                  containerStyle={{margin: 30, backgroundColor: 'pink', padding: 10}}
+                  avatarStyle={{ width: 50, height: 50}}
+                  onPress={() => console.log(item.body)}
+                  title={item.title}
+                  avatar={item.avatar_url}
+                />
+              )}
+            />
+        </List>
     </View>
   );
 };
+
+
 
 const styles = StyleSheet.create({
   container: {
